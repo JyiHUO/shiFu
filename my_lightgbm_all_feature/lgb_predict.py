395faa28f9c247c07b97b6d71cbf08071a1ab057
JyiHUO <879837607@qq.com>
2019-03-26 13:13:37 +0800

    some bug in lgb predict

--- my_lightgbm_all_feature/lgb_predict.py
+++ my_lightgbm_all_feature/lgb_predict.py
@@ -30,32 +30,32 @@ def lgb_predict_media():
             clf = pickle.load(f)
             like_model.append(clf)
 
-        for fm in finish_model:
-            pred = fm.predict_proba(test_data)
-            final_pred_list.append(pred[:, 1][:, None])
-            print(pred.shape)
+    for fm in finish_model:
+        pred = fm.predict_proba(test_data)
+        final_pred_list.append(pred[:, 1][:, None])
+        print(pred.shape)
 
-        final_pred_list = np.concatenate(final_pred_list, 1)
-        for lm in like_model:
-            pred = lm.predict_proba(test_data)
-            like_pred_list.append(pred[:, 1][:, None])
-            print(pred.shape)
-        like_pred_list = np.concatenate(like_pred_list, 1)
+    final_pred_list = np.concatenate(final_pred_list, 1)
+    for lm in like_model:
+        pred = lm.predict_proba(test_data)
+        like_pred_list.append(pred[:, 1][:, None])
+        print(pred.shape)
+    like_pred_list = np.concatenate(like_pred_list, 1)
 
-        big_result = np.concatenate([uid[:, None], item_id[:, None],
-                                     final_pred_list, like_pred_list], 1)
-        result = np.concatenate([uid[:, None], item_id[:, None],
-                                 np.mean(final_pred_list, 1, keepdims=True),
-                                 np.mean(like_pred_list, 1, keepdims=True)], 1)
-        big_result_columns = ["uid", "item_id"]
-        big_result_columns.extend(["finish_probability_"+str(1) for i in range(1, 8)])
-        big_result_columns.extend(["like_probability_"+str(1) for i in range(1, 8)])
-        result_columns = ["uid", "item_id","finish_probability", "like_probability"]
-        big_result = pd.DataFrame(big_result, columns=big_result_columns)
-        result = pd.DataFrame(result, columns=result_columns)
-        big_result.to_csv(Config["normal_config"]["predict_file"]+"_big_result",
-                          index=None, float_format="%.6f")
-        result.to_csv(Config["normal_config"]["predict_file"] + "_result",
+    big_result = np.concatenate([uid[:, None], item_id[:, None],
+                                 final_pred_list, like_pred_list], 1)
+    result = np.concatenate([uid[:, None], item_id[:, None],
+                             np.mean(final_pred_list, 1, keepdims=True),
+                             np.mean(like_pred_list, 1, keepdims=True)], 1)
+    big_result_columns = ["uid", "item_id"]
+    big_result_columns.extend(["finish_probability_"+str(1) for i in range(1, 8)])
+    big_result_columns.extend(["like_probability_"+str(1) for i in range(1, 8)])
+    result_columns = ["uid", "item_id","finish_probability", "like_probability"]
+    big_result = pd.DataFrame(big_result, columns=big_result_columns)
+    result = pd.DataFrame(result, columns=result_columns)
+    big_result.to_csv(Config["normal_config"]["predict_file"]+"_big_result",
                       index=None, float_format="%.6f")
+    result.to_csv(Config["normal_config"]["predict_file"] + "_result",
+                  index=None, float_format="%.6f")
 
 lgb_predict_media()
